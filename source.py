09ce65df3d7f3f6f57e9fcdc88c9bb809f1c420b
Elijah Flinders Desktop <elijah.flinders@mines.sdsmt.edu>
2020-07-16 10:22:50 -0600

    made the json output pretty

--- source.py
+++ source.py
@@ -4,10 +4,12 @@ from requests.exceptions import HTTPError
 #API Key: 40C4A06852CCC51C4B062714DA4478C5
 
 try: 
-    dataFetch = requests.get(' http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=40C4A06852CCC51C4B062714DA4478C5&steamids=76561198045755215')
+    dataFetch = requests.get('http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=40C4A06852CCC51C4B062714DA4478C5&steamids=76561198045755215')
+    #dataFetch = requests.get('http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key=40C4A06852CCC51C4B062714DA4478C5&vanityurl=lijah99')
     dataFetch.raise_for_status()
+    
     #access json
-    fetchedJson = dataFetch.json()
+    fetchedJson = json.dumps(json.loads(dataFetch.content), indent=2)
     print("Entire JSON retrieval")
     print(fetchedJson)
 
