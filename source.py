48dfcaefd3309ba16949341e3ee9b0e3e737b2e3
Elijah Flinders Desktop <elijah.flinders@mines.sdsmt.edu>
2020-07-21 15:39:01 -0600

    Added API connection to owned steam games

--- source.py
+++ source.py
@@ -4,13 +4,19 @@ from requests.exceptions import HTTPError
 #API Key: 40C4A06852CCC51C4B062714DA4478C5
 
 try: 
-    dataFetch = requests.get('http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=40C4A06852CCC51C4B062714DA4478C5&steamids=76561198045755215')
+    dataFetch = requests.get('http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=40C4A06852CCC51C4B062714DA4478C5&steamids=76561198031865653')
     #dataFetch = requests.get('http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key=40C4A06852CCC51C4B062714DA4478C5&vanityurl=lijah99')
     dataFetch.raise_for_status()
     
-    #access json
+    #access json for user summary
     fetchedJson = json.dumps(json.loads(dataFetch.content), indent=2)
-    print("Entire JSON retrieval")
+    print("Entire JSON retrieval for user summary")
+    print(fetchedJson)
+
+    #access json for user owned games
+    dataFetch = requests.get('http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=40C4A06852CCC51C4B062714DA4478C5&include_played_free_games=1&include_appinfo=1&format=json&steamid=76561198031865653')
+    fetchedJson = json.dumps(json.loads(dataFetch.content), indent=2)
+    print("Entire JSON retrieval for user owned games")
     print(fetchedJson)
 
 except HTTPError as http_err:
